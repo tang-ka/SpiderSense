bf97fcb8833aa3efc60092e225c89a39e51a33e9
tang-ka <gur0907@hanyang.ac.kr>
2022-07-29 01:39:54 +0900

    .

--- Assets/Script_ssh/CamRotate.cs
+++ Assets/Script_ssh/CamRotate.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class CamRotate : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float sensX = 500;
+    public float sensY = 500;
+
+    float xRotation;
+    float yRotation;
+
     void Start()
     {
-        
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float mouseX = Input.GetAxisRaw("Mouse X") * Time.deltaTime * sensX;
+        float mouseY = Input.GetAxisRaw("Mouse Y") * Time.deltaTime * sensY;
+
+        yRotation += mouseX;
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -85f, 85f);
+
+        transform.eulerAngles = new Vector3(xRotation, yRotation, 0);
     }
 }
--- Assets/Script_ssh/PlayerMove.cs
+++ Assets/Script_ssh/PlayerMove.cs
@@ -2,17 +2,40 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// 입력을 받아 이동하고 싶다.
 public class PlayerMove : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float walkSpeed = 10;
+    float speed = 0;
+    float time = 0;
+
+    Rigidbody rb;
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
     void Update()
     {
+        float v = Input.GetAxisRaw("Vertical");
+        float h = Input.GetAxisRaw("Horizontal");
+
+        Vector3 dir = Vector3.forward * v + Vector3.right * h;
+        dir.Normalize();
+
         
+        if (v != 0 || h != 0)
+        {
+            time += Time.deltaTime * 10;
+            speed = Mathf.Lerp(5, walkSpeed, time);
+        }
+        else
+        {
+            time = 0;
+        }
+
+        print(speed + "     " + time);
+        rb.velocity = dir * speed;
     }
 }
