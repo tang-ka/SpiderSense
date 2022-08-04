b5aa1c50d0504b388c927c97e3c8229d5736704a
tang-ka <gur0907@hanyang.ac.kr>
2022-08-04 19:09:05 +0900

    .

--- Assets/SSH/SSH_Script/SSH_SpiderMove.cs
+++ Assets/SSH/SSH_Script/SSH_SpiderMove.cs
@@ -7,22 +7,27 @@ public class SSH_SpiderMove : MonoBehaviour
     public Transform hook;
     public Transform rightHand;
 
+    Vector3 defaultHookPosition;
+    Vector3 grapHookPosition;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.LeftShift))
+        if (Input.GetKey(KeyCode.E))
         {
             Vector3 webDir = hook.position - rightHand.position;
             float webLen = Vector3.Distance(rightHand.position, hook.position);
             Ray web = new Ray(rightHand.position, webDir);
             Debug.DrawRay(web.origin, web.direction * webLen, Color.white);
-            print(111);
+
+
+
         }
     }
 }
