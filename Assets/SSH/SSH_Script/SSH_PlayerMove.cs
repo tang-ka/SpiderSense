ec493014d048c62096e565f77c662d893fab4b5e
tang-ka <gur0907@hanyang.ac.kr>
2022-08-05 14:59:20 +0900

    .

--- Assets/SSH/SSH_Script/SSH_PlayerMove.cs
+++ Assets/SSH/SSH_Script/SSH_PlayerMove.cs
@@ -11,6 +11,8 @@ public class SSH_PlayerMove : MonoBehaviour
 
     Rigidbody rb;
     SSH_SpiderMove spiderMovement;
+    SSH_CamPivotRotate cpr;
+    float yRotation;
     public Transform camDir;
 
     float gravity = -9.81f;
@@ -31,64 +33,103 @@ public class SSH_PlayerMove : MonoBehaviour
     {
         rb = GetComponent<Rigidbody>();
         spiderMovement = GetComponent<SSH_SpiderMove>();
+        cpr = GetComponentInChildren<SSH_CamPivotRotate>();
     }
 
     void Update()
     {
         StateManage();
-        Movement();
     }
 
     private void StateManage()
     {
         if (state == PlayerState.Normal)
         {
-            NormalInput();
+            Movement();
+            if (Input.GetKey(KeyCode.E))
+                state = PlayerState.Webbing;
         }
         else if (state == PlayerState.Webbing)
         {
-            WebbingInput();
+            WebSwing();
+            if (Input.GetKeyUp(KeyCode.E))
+                state = PlayerState.Normal;
         }
     }
 
-    private void WebbingInput()
+    public Transform hook;
+    public Transform rightHand;
+
+    Vector3 defaultHookPosition;
+    Vector3 grapHookPosition;
+
+    public Transform body;
+    private void WebSwing()
     {
-        Rotate();
+        //transform.forward = camDir.forward;
 
-        float v = Input.GetAxisRaw("Vertical");
+        if (Input.GetKey(KeyCode.E))
+        {
+            Vector3 webDir = hook.position - rightHand.position;
+            float webLen = Vector3.Distance(rightHand.position, hook.position);
 
-        dir = transform.forward * v;
-        dir.Normalize();
+            Ray web = new Ray(rightHand.position, webDir);
+            Debug.DrawRay(web.origin, web.direction * webLen, Color.white);
+
+            RaycastHit pivot;
+            if (Physics.Raycast(web, out pivot, webLen))
+            {
+                Debug.DrawRay(transform.position, transform.up * webLen, Color.red);
+                // 거미줄 
+                transform.up = web.direction;
+
+                float mouseX = Input.GetAxisRaw("Mouse X") * cpr.sensX * Time.deltaTime;
+                yRotation += mouseX;
+                body.localEulerAngles = new Vector3(0, yRotation, 0);
+
+                //float theta = Vector3.Angle(-transform.up, Vector3.down);
+                //dir = transform.forward + transform.up;
+                //float speed = gravity * Mathf.Sin(theta * Mathf.Deg2Rad);
+
+                //float v = Input.GetAxisRaw("Vertical");
+
+                //dir = transform.forward * v + transform.up;
+                //dir.Normalize();
+            }
+        }
 
         Jump();
+
+        rb.velocity = dir * walkSpeed;
+
+        if (Input.GetKeyUp(KeyCode.E))
+        {
+            transform.up = Vector3.up;
+        }
     }
 
-    private void NormalInput()
+    void Movement()
     {
-        // 일반적인 입력을 받아 이동하고 싶다.
         Rotate();
 
         float v = Input.GetAxisRaw("Vertical");
         float h = Input.GetAxisRaw("Horizontal");
 
-        dir = transform.forward * v + transform.right * h;
+        dir = body.forward * v + body.right * h;
         dir.Normalize();
 
         Jump();
-    }
 
-    void Movement()
-    {
         rb.velocity = dir * walkSpeed;
     }
 
     // 플레이어가 보는 방향을 카메라의 방향과 맞춰준다.
     void Rotate()
     {
-        Vector3 playerForward = camDir.forward;
+        Vector3 playerForward = camDir.transform.forward;
         playerForward.y = 0;
 
-        transform.forward = playerForward;
+        body.forward = playerForward;
     }
 
     void Jump()
@@ -113,13 +154,13 @@ public class SSH_PlayerMove : MonoBehaviour
     {
         Ray ray = new Ray(transform.position + transform.up, -transform.up);
 
-        Debug.DrawRay(ray.origin, ray.direction * 1.1f, Color.red);
+        Debug.DrawRay(ray.origin, ray.direction * 1.2f, Color.red);
         RaycastHit hitInfo;
 
         if (Physics.Raycast(ray, out hitInfo))
         {
             
-            if (hitInfo.distance < 1.1f && yVelocity <= 0)
+            if (hitInfo.distance < 1.2f && yVelocity <= 0)
             {
                 isJumping = false;
             }
@@ -128,5 +169,19 @@ public class SSH_PlayerMove : MonoBehaviour
         }
         else
             isJumping = true;
+    }
+
+    private void NormalInput()
+    {
+        // 일반적인 입력을 받아 이동하고 싶다.
+        Rotate();
+
+        float v = Input.GetAxisRaw("Vertical");
+        float h = Input.GetAxisRaw("Horizontal");
+
+        dir = transform.forward * v + transform.right * h;
+        dir.Normalize();
+
+        Jump();
     }
 }
--- Assets/SSH/SSH_Script/SSH_SpiderMove.cs
+++ Assets/SSH/SSH_Script/SSH_SpiderMove.cs
@@ -23,11 +23,40 @@ public class SSH_SpiderMove : MonoBehaviour
         {
             Vector3 webDir = hook.position - rightHand.position;
             float webLen = Vector3.Distance(rightHand.position, hook.position);
+
             Ray web = new Ray(rightHand.position, webDir);
-            Debug.DrawRay(web.origin, web.direction * webLen, Color.white);
+            Debug.DrawRay(web.origin, web.direction * webLen, Color.black);
 
-
-
+            RaycastHit pivot;
+            if (Physics.Raycast(web, out pivot, webLen))
+            {
+                Debug.DrawRay(transform.position, transform.up * webLen, Color.red);
+                transform.up = web.direction;
+            }
+            if (Input.GetKeyUp(KeyCode.E))
+            {
+                transform.up = Vector3.up;
+            }
         }
     }
+
+    //Vector3 WebSwing()
+    //{
+    //    if (Input.GetKey(KeyCode.E))
+    //    {
+    //        Vector3 webDir = hook.position - rightHand.position;
+    //        float webLen = Vector3.Distance(rightHand.position, hook.position);
+
+    //        Ray web = new Ray(rightHand.position, webDir);
+    //        Debug.DrawRay(web.origin, web.direction * webLen, Color.black);
+
+    //        RaycastHit pivot;
+    //        if (Physics.Raycast(web, out pivot))
+    //        {
+    //            transform.up = web.direction;
+
+    //        }
+
+    //    }
+    //}
 }
