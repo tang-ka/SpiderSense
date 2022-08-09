ea5e888eab1c939f6a44ed943a16b63c83c22280
ssb5783 <110151488+ssb5783@users.noreply.github.com>
2022-08-09 17:39:37 +0900

    .

--- Assets/SSB/SB_Scripts/SB_Jet_Move.cs
+++ Assets/SSB/SB_Scripts/SB_Jet_Move.cs
@@ -4,7 +4,14 @@ using UnityEngine;
 
 public class SB_Jet_Move : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    //Jet가 플레이어의 주변을 날아다니게 하고싶다.
+    //랜덤값을 정하고,Player의 position에 랜덤값을 더한다
+    //랜덤값
+    float randomic = Random.Range(0,10);
+    //플레이어의 Position값
+    Vector3 playerPos = GameObject.Find("Player").transform.position;
+    
     void Start()
     {
         
@@ -13,6 +20,8 @@ public class SB_Jet_Move : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //randomic값을 플레이어 포지션에 더해서 현재값에 넣어준다
+        
         
     }
 }
