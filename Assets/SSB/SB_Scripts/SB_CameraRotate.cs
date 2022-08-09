366d6cbb8900e93636af54a4e75493fe597c54bf
ssb5783 <110151488+ssb5783@users.noreply.github.com>
2022-08-09 17:32:47 +0900

    ..

--- Assets/SSB/SB_Scripts/SB_CameraRotate.cs
+++ Assets/SSB/SB_Scripts/SB_CameraRotate.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class KKKK : MonoBehaviour
+public class SB_CameraRotate : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
