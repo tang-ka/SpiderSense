d45b1438de899951f48da4d46b6ce347feccbea9
tang-ka <gur0907@hanyang.ac.kr>
2022-08-19 14:09:49 +0900

    ..

--- Assets/SSH/SSH_UI/SSH_ImageChange.cs
+++ Assets/SSH/SSH_UI/SSH_ImageChange.cs
@@ -18,18 +18,44 @@ public class SSH_ImageChange : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        int imageNum = Random.Range(0, 10);
+        int imageNum = Random.Range(0, 9);
 
         currentTime += Time.deltaTime;
 
         switch (imageNum)
         {
             case 0:
-                if (currentTime > endTime)
-                {
+                ShowImage();
+                break;
 
-                    break;
-                }
+            case 1:
+                break;
+
+            case 2:
+                break;
+
+            case 3:
+                break;
+
+            case 4:
+                break;
+
+            case 5:
+                break;
+
+            case 6:
+                break;
+
+            case 7:
+                break;
+
+            case 8:
+                break;
         }
+    }
+
+    void ShowImage()
+    {
+
     }
 }
